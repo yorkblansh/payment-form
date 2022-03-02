cf238b34446546902422b2acffe67d03530243a5
york blansh <yorkblansh@gmail.com>
2022-03-02 13:35:40 +0300

    regexp

--- react_frontend/src/app/hooks/useValidate.ts
+++ react_frontend/src/app/hooks/useValidate.ts
@@ -2,9 +2,15 @@ import { INPUT_MAP } from 'components/inputs/input';
 import { useState } from 'react';
 import validator from 'validator';
 
+const re = (str: string) => (re: RegExp) => re.test(str);
+
 export const useValidate = () => {
 	const [value, checkValue] = useState<{ val: string; TYPE: keyof typeof INPUT_MAP }>({ val: '', TYPE: 'card' });
 	const { TYPE, val } = value;
+	const card = TYPE === 'card',
+		amount = TYPE === 'amount',
+		expdate = TYPE === 'expdate',
+		cvv = TYPE === 'cvv';
 
 	let _value;
 	if (val !== undefined) {
@@ -18,12 +24,28 @@ export const useValidate = () => {
 			if (validator.isNumeric(value.val)) _value = val;
 			else _value = val.substr(0, val.length - 1);
 
-		if (TYPE === 'cvv')
-			if (validator.isNumeric(val))
-				if (val.length <= 3) _value = val;
-				else _value = val.substr(0, val.length - 1);
-			else _value = val.substr(0, val.length - 1);
-		// if (TYPE === 'expdate')
+		if (TYPE === 'cvv') re(val)(/\b\d{1,3}\b/) ? (_value = val) : (_value = val.substr(0, val.length - 1));
+		if (TYPE === 'expdate') {
+			// if (/\d/.test(val) && val.length <= 2) {
+			// 	_value = val.replace(/\d\d/, `${val}/`);
+			// } else {
+			// 	_value = val.substr(0, val.length - 1);
+			// 	if (/\d\d[/]\d/.test(val) && val.length <= 5) {
+			// 		_value = val.replace(/\d\d[/]\d\d/, `${val}/`);
+			// 	}
+			// }
+			// if (/(0?[1-9])[/]\d/.test(val) && val.length <= 5) {
+			// 	if (val.length < 5) {
+			// 		_value = val;
+			// 	}
+			// 	// else _value = val.replace(/\d\d[/]\d\d/, `${val}/`);
+			// }
+			// if(/(0?[1-9])/)
+			// if (/\d/.test(val)) {
+			// 	_value = val;
+			// 	// /\d\d/.test(val) ? (_value = val.replace(/\d\d/, `${val}/`)) : (_value = val.substr(0, val.length - 1));
+			// } else _value = val.substr(0, val.length - 1);
+		}
 		// 	if (validator.isNumeric(val))
 		// 		if (val.length <= 2) _value = `${val}/`;
 		// 		else _value = val.substr(0, val.length - 1);
