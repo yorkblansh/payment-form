647467a6fb66949be52c11a04d9d5eb923cdff33
york blansh <yorkblansh@gmail.com>
2022-03-07 02:04:11 +0300

    before final check

--- react_frontend/src/index.tsx
+++ react_frontend/src/index.tsx
@@ -1,18 +1,17 @@
-import ReactDOM from 'react-dom';
-import './index.scss';
-import { MAIN_PAGE } from 'PAGES/MAIN_PAGE/MAIN_PAGE';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { PATH } from 'api/consts';
+import ReactDOM from 'react-dom'
+import './index.scss'
+import { MAIN_PAGE } from 'PAGES/MAIN_PAGE/MAIN_PAGE'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const App = () => (
 	<BrowserRouter>
 		<Routes>
-			<Route   path={PATH('/')} element={<MAIN_PAGE />} />
+			<Route path="/" element={<MAIN_PAGE />} />
 		</Routes>
 	</BrowserRouter>
-);
+)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
